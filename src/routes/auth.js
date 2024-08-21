0b9b93ba74ae906c96636d058528140b32c1ed45
hamza benarfa <hamzabenarfa4@gmail.com>
2024-08-21 22:17:18 +0100

    cleaned files

--- src/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-const { register } = require("../controller/auth");
-
-router.post("/register", register);
-
-//router.post("/login", login);
-
-module.exports = router;
