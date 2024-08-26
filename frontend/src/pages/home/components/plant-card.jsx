40438ad639e72b7372f261f6d8c5ddcf7c2ef818
hamza benarfa <hamzabenarfa4@gmail.com>
2024-08-26 21:07:15 +0100

    made hero section , made navbar , made logo component, made pages responsive

--- frontend/src/pages/home/components/plant-card.jsx
+++ frontend/src/pages/home/components/plant-card.jsx
@@ -1,17 +1,18 @@
 import Button from "./button-buy";
+import PlantButton from "./plant-button";
 
-const PlantCard = ({name,type,price,plantImage}) => {
+const PlantCard = ({ name, type, price, plantImage }) => {
   return (
-    <div className=" p-4 shadow-lg rounded-xl w-80">
+    <div className="p-4 flex flex-col items-start shadow-lg rounded-xl w-80">
       <img
         src={`http://localhost:4000/${plantImage}`}
         alt="name"
-        className=" rounded-xl"
+        className="rounded-xl size-72 object-cover"
       />
-      <h1 className="text-2xl font-bold">{name}</h1>
-      <p className=" text-gray-400 ">{type}</p>
-      <p className=" font-bold">{price}</p>
-      <Button name="buy" />
+      <h1 className=" capitalize text-4xl font-bold">{name}</h1>
+      <p className=" text-xs text-gray-400 ">{type}</p>
+      <p className=" font-bold">{price} TND</p>
+      <PlantButton name="Shop Now" />
     </div>
   );
 };
--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -2,6 +2,7 @@ import PlantCard from "./components/plant-card";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Navbar from "../../components/nav-bar";
+import Hero from "./components/hero";
 function Home() {
   const [plants, setPlants] = useState([]);
   async function fetchPlants() {
@@ -14,38 +15,21 @@ function Home() {
   }, []);
 
   return (
-    <div className="flex flex-col gap-4  min-h-screen p-4 ">
+    <div className="flex flex-col gap-4 min-h-screen p-4 w-full ">
       <Navbar />
-
-      <section className="flex flex-row justify-between">
-        <div className="flex flex-col items-start">
-          <h1 className="text-[#21441f] font-bold text-4xl ">
-            Various Indoor <br /> Plant shop
-          </h1>
-          <p className=" font-semibold text-zinc-400 max-w-xs">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
-            alias aperiam esse eius assumenda sunt est quasi, molestias quae
-            pariatur, soluta a culpa quis incidunt quos delectus ipsum corporis
-            deleniti.
-          </p>
-          <button>Add To Cart</button>
-        </div>
-        <img
-          src="images/plant-hero-img-.png"
-          className=" w-40"
-          alt="plant-hero-img"
-        />
-      </section>
-
-      {/* {plants.map((plant) => (
-        <PlantCard
-          key={plant.id}
-          name={plant.name}
-          type={plant.type}
-          price={plant.price}
-          plantImage={plant.plantImage}
-        />
-      ))} */}
+      <Hero />
+      <div className="md:px-40 flex flex-col 
+        md:flex-row items-center md:flex-wrap gap-4">
+        {plants && plants.map((plant) => (
+          <PlantCard
+            key={plant.id}
+            name={plant.name}
+            type={plant.type}
+            price={plant.price}
+            plantImage={plant.plantImage}
+          />
+        ))}
+      </div>
     </div>
   );
 }
