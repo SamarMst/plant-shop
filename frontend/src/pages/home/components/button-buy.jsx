e088d27fb6a187402fed00c0cdeb6bc5459a3983
hamza benarfa <hamzabenarfa4@gmail.com>
2024-08-26 21:07:15 +0100

    made hero section , made navbar , made logo component, made pages responsive

--- frontend/src/pages/home/components/button-buy.jsx
+++ frontend/src/pages/home/components/button-buy.jsx
@@ -1,6 +1,6 @@
 const Button = ({name}) => {
   return (
-    <div className="relative inline-block px-14 py-2 font-medium group ">
+    <div className="relative inline-block px-20 py-2 font-medium group ">
       <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
       <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
       <span className="relative text-black group-hover:text-white">
