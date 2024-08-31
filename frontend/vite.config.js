4d5b3f299e5242b4e62c133ff2072a3a81bbfaad
hamza benarfa <hamzabenarfa4@gmail.com>
2024-08-31 22:02:57 +0100

    fix err vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,15 @@
-// vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+console.log(path.resolve(__dirname, './src'));
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      '@': path.resolve(__dirname, './src'), // Add this line to map '@' to 'src'
     },
+  },
+  define: {
+    "process.env": {},
   },
 });
