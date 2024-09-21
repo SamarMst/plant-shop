b246cf5d7fb5a9e6e4a070a53907078a179f0816
samar_mestiri <samar002ms@gmail.com>
2024-09-21 22:01:20 +0100

    updated  payment and home component

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -7,6 +7,7 @@ import axiosInstance from "@/lib/axios-instance";
 
 function Home() {
   const [plants, setPlants] = useState([]);
+  const [visiblePlants, setVisiblePlants] = useState(3);
 
   async function fetchPlants() {
     try {
@@ -21,23 +22,36 @@ function Home() {
     fetchPlants();
   }, []);
 
+  const handleShowMore = () => {
+    setVisiblePlants((prev) => prev + 3);
+  };
+
   return (
     <div className="flex flex-col gap-4 min-h-screen w-full">
       <Navbar />
       <Hero />
-      <div className="md:px-40 flex flex-col md:flex-row items-center md:flex-wrap gap-4">
-        {plants &&
-          plants.map((plant) => (
-            <PlantCard
-              id={plant.id}
-              key={plant.id}
-              name={plant.name}
-              type={plant.type}
-              price={plant.price}
-              plantImage={plant.resources[0]?.filename}
-            />
-          ))}
+      <div className="md:px-40 flex flex-col md:flex-row justify-center items-center md:flex-wrap gap-4">
+        {plants.slice(0, visiblePlants).map((plant) => (
+          <PlantCard
+            id={plant.id}
+            key={plant.id}
+            name={plant.name}
+            type={plant.type}
+            price={plant.price}
+            plantImage={plant.resources[0]?.filename}
+          />
+        ))}
       </div>
+      {visiblePlants < plants.length && (
+        <div className="flex justify-center mt-4">
+          <button
+            onClick={handleShowMore}
+            className="text-lg font-sans bg-black text-white px-4 py-2 rounded-md hover:bg-gray-900 transition-colors duration-300"
+          >
+            Show More
+          </button>
+        </div>
+      )}
       <footer className="flex flex-row justify-between items-center px-6 py-4 border-t-2 shadow-md">
         <Logo />
         <p>Copyright 2024</p>
