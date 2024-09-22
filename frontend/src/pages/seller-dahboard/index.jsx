06914da1ca1fe68bca992028849811ce1ba8b509
samar_mestiri <samar002ms@gmail.com>
2024-09-22 18:07:21 +0100

    seller info  component

--- frontend/src/pages/seller-dahboard/index.jsx
+++ frontend/src/pages/seller-dahboard/index.jsx
@@ -2,22 +2,36 @@ import { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./components/sideBar";
 import useGetUserInfo from "@/hook/useGetUserInfo";
-import useFetchMyPlants from "@/hook/useFetchMyPlants";
-import Toast from "react-hot-toast";
+import axiosInstance from "@/lib/axios-instance";
 
 const SellerDashboard = () => {
-  //const { plants, error } = useFetchMyPlants();
-  const { email } = useGetUserInfo();
-  console.log("🚀 ~ SellerDashboard ~ email:", email);
-  //console.log("🚀 ~ SellerDashboard ~ plants:", plants);
-  //if (error) return Toast.error(error);
+  const { id } = useGetUserInfo();
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const response = await axiosInstance.get(`/user/${id}/info`);
+        const { name, lastname } = response.data;
+        setName(name);
+        setLastName(lastname);
+      } catch (error) {
+        console.error("Error fetching user info:", error);
+      }
+    };
+
+    fetchUserInfo();
+  }, [id]);
 
   return (
     <div className="flex h-screen">
       <Sidebar />
       <div className="flex flex-col flex-grow">
         <div className="bg-[#10b981] text-white p-4 flex justify-between items-center">
-          <div className="text-lg font-bold">Welcome, {email}!</div>
+          <div className="text-lg font-bold">
+            Welcome, {name} {lastName} !
+          </div>
         </div>
         <div className="p-6 flex-grow overflow-auto ">
           <Outlet />
