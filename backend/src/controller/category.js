4d63e798c148d4bc6934f6e7c262c48e16bf8006
samar_mestiri <samar002ms@gmail.com>
2024-09-23 21:06:49 +0100

    updated category controller

--- backend/src/controller/category.js
+++ backend/src/controller/category.js
@@ -24,6 +24,43 @@ const createCategory = async (req, res) => {
   }
 };
 
+const updateCategory = async (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  try {
+    if (!name) {
+      return res.status(400).json({ message: "Empty field" });
+    }
+
+    const category = await prisma.plantCategory.findUnique({
+      where: { id: Number(id) },
+    });
+
+    if (!category) {
+      return res.status(400).json({ message: "Category does not exist" });
+    }
+
+    const existingCategory = await prisma.plantCategory.findUnique({
+      where: { name },
+    });
+
+    if (existingCategory) {
+      return res
+        .status(400)
+        .json({ message: "Category with this name already exists" });
+    }
+    await prisma.plantCategory.update({
+      where: { id: Number(id) },
+      data: { name },
+    });
+
+    res.status(200).json({ message: "Category updated successfully" });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
 const getCategory = async (req, res) => {
   try {
     const category = await prisma.plantCategory.findMany();
@@ -36,4 +73,5 @@ const getCategory = async (req, res) => {
 module.exports = {
   createCategory,
   getCategory,
+  updateCategory,
 };
